6401461e593185f09546073e389f0bd9605cea76
jones <mobilizacsio@gmail.com>
2022-11-22 00:06:22 +0100

    Azimuth slider done

--- SVN-constellation/svn/src/app/elevation-azimuth/elevation-azimuth.component.ts
+++ SVN-constellation/svn/src/app/elevation-azimuth/elevation-azimuth.component.ts
@@ -12,11 +12,33 @@ export class ElevationAzimuthComponent implements OnInit {
 
   ngOnInit(): void { 
 
-    $("#slider").roundSlider({
-    radius: 85,
-    sliderType: "default",
-    value: 24
+    $("#circle").roundSlider({
+      sliderType: "min-range",
+      circleShape: "full",
+      startAngle: "-90",
+      lineCap: "round",
+      radius: 100,
+      width: 3,
+      handleSize: "+10",
+      handleShape: "round",
+      min: 60,
+      max: -60,
+      
+      svgMode: true,
+      pathColor: "#2B2D30",
+      borderWidth: 0,
+      
+      startValue: 0,
+      
+      valueChange: function (item: any) {
+        let color = item.isInvertedRange ? "#6BDBEB" : "#6BDBEB";
+        
+        $("#circle").roundSlider({ "rangeColor": color, "tooltipColor": color });
+      }
     });
+  
+  let sliderObj = $("#circle").data("roundSlider");
+  sliderObj.setValue(0);
    }  
 
 
